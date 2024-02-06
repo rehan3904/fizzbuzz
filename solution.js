ebf6a16047a7152f3243ecdb93fb5ae8ad3348c6
rehan3904 <159200126+rehan3904@users.noreply.github.com>
2024-02-06 17:50:59 -0500

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,304 @@
+fizzbuzz(42);
+
+function fizzbuzz(i) {
+    if (i == 1) {
+        console.log(1);
+    } else
+    if (i == 2) {
+        console.log(2);
+    } else
+    if (i == 3) {
+        console.log("Fizz");
+    } else
+    if (i == 4) {
+        console.log(4);
+    } else
+    if (i == 5) {
+        console.log("Buzz");
+    } else
+    if (i == 6) {
+        console.log("Fizz");
+    } else
+    if (i == 7) {
+        console.log(7);
+    } else
+    if (i == 8) {
+        console.log(8);
+    } else
+    if (i == 9) {
+        console.log("Fizz");
+    } else
+    if (i == 10) {
+        console.log("Buzz");
+    } else
+    if (i == 11) {
+        console.log(11);
+    } else
+    if (i == 12) {
+        console.log("Fizz");
+    } else
+    if (i == 13) {
+        console.log(13);
+    } else
+    if (i == 14) {
+        console.log(14);
+    } else
+    if (i == 15) {
+        console.log("FizzBuzz");
+    } else
+    if (i == 16) {
+        console.log(16);
+    } else
+    if (i == 17) {
+        console.log(17);
+    } else
+    if (i == 18) {
+        console.log("Fizz");
+    } else
+    if (i == 19) {
+        console.log(19);
+    } else
+    if (i == 20) {
+        console.log("Buzz");
+    } else
+    if (i == 21) {
+        console.log("Fizz");
+    } else
+    if (i == 22) {
+        console.log(22);
+    } else
+    if (i == 23) {
+        console.log(23);
+    } else
+    if (i == 24) {
+        console.log("Fizz");
+    } else
+    if (i == 25) {
+        console.log("Buzz");
+    } else
+    if (i == 26) {
+        console.log(26);
+    } else
+    if (i == 27) {
+        console.log("Fizz");
+    } else
+    if (i == 28) {
+        console.log(28);
+    } else
+    if (i == 29) {
+        console.log(29);
+    } else
+    if (i == 30) {
+        console.log("FizzBuzz");
+    } else
+    if (i == 31) {
+        console.log(31);
+    } else
+    if (i == 32) {
+        console.log(32);
+    } else
+    if (i == 33) {
+        console.log("Fizz");
+    } else
+    if (i == 34) {
+        console.log(34);
+    } else
+    if (i == 35) {
+        console.log("Buzz");
+    } else
+    if (i == 36) {
+        console.log("Fizz");
+    } else
+    if (i == 37) {
+        console.log(37);
+    } else
+    if (i == 38) {
+        console.log(38);
+    } else
+    if (i == 39) {
+        console.log("Fizz");
+    } else
+    if (i == 40) {
+        console.log("Buzz");
+    } else
+    if (i == 41) {
+        console.log(41);
+    } else
+    if (i == 42) {
+        console.log("Fizz");
+    } else
+    if (i == 43) {
+        console.log(43);
+    } else
+    if (i == 44) {
+        console.log(44);
+    } else
+    if (i == 45) {
+        console.log("FizzBuzz");
+    } else
+    if (i == 46) {
+        console.log(46);
+    } else
+    if (i == 47) {
+        console.log(47);
+    } else
+    if (i == 48) {
+        console.log("Fizz");
+    } else
+    if (i == 49) {
+        console.log(49);
+    } else
+    if (i == 50) {
+        console.log("Buzz");
+    } else
+    if (i == 51) {
+        console.log("Fizz");
+    } else
+    if (i == 52) {
+        console.log(52);
+    } else
+    if (i == 53) {
+        console.log(53);
+    } else
+    if (i == 54) {
+        console.log("Fizz");
+    } else
+    if (i == 55) {
+        console.log("Buzz");
+    } else
+    if (i == 56) {
+        console.log(56);
+    } else
+    if (i == 57) {
+        console.log("Fizz");
+    } else
+    if (i == 58) {
+        console.log(58);
+    } else
+    if (i == 59) {
+        console.log(59);
+    } else
+    if (i == 60) {
+        console.log("FizzBuzz");
+    } else
+    if (i == 61) {
+        console.log(61);
+    } else
+    if (i == 62) {
+        console.log(62);
+    } else
+    if (i == 63) {
+        console.log("Fizz");
+    } else
+    if (i == 64) {
+        console.log(64);
+    } else
+    if (i == 65) {
+        console.log("Buzz");
+    } else
+    if (i == 66) {
+        console.log("Fizz");
+    } else
+    if (i == 67) {
+        console.log(67);
+    } else
+    if (i == 68) {
+        console.log(68);
+    } else
+    if (i == 69) {
+        console.log("Fizz");
+    } else
+    if (i == 70) {
+        console.log("Buzz");
+    } else
+    if (i == 71) {
+        console.log(71);
+    } else
+    if (i == 72) {
+        console.log("Fizz");
+    } else
+    if (i == 73) {
+        console.log(73);
+    } else
+    if (i == 74) {
+        console.log(74);
+    } else
+    if (i == 75) {
+        console.log("FizzBuzz");
+    } else
+    if (i == 76) {
+        console.log(76);
+    } else
+    if (i == 77) {
+        console.log(77);
+    } else
+    if (i == 78) {
+        console.log("Fizz");
+    } else
+    if (i == 79) {
+        console.log(79);
+    } else
+    if (i == 80) {
+        console.log("Buzz");
+    } else
+    if (i == 81) {
+        console.log("Fizz");
+    } else
+    if (i == 82) {
+        console.log(82);
+    } else
+    if (i == 83) {
+        console.log(83);
+    } else
+    if (i == 84) {
+        console.log("Fizz");
+    } else
+    if (i == 85) {
+        console.log("Buzz");
+    } else
+    if (i == 86) {
+        console.log(86);
+    } else
+    if (i == 87) {
+        console.log("Fizz");
+    } else
+    if (i == 88) {
+        console.log(88);
+    } else
+    if (i == 89) {
+        console.log(89);
+    } else
+    if (i == 90) {
+        console.log("FizzBuzz");
+    } else
+    if (i == 91) {
+        console.log(91);
+    } else
+    if (i == 92) {
+        console.log(92);
+    } else
+    if (i == 93) {
+        console.log("Fizz");
+    } else
+    if (i == 94) {
+        console.log(94);
+    } else
+    if (i == 95) {
+        console.log("Buzz");
+    } else
+    if (i == 96) {
+        console.log("Fizz");
+    } else
+    if (i == 97) {
+        console.log(97);
+    } else
+    if (i == 98) {
+        console.log(98);
+    } else
+    if (i == 99) {
+        console.log("Fizz");
+    } else
+    if (i == 100) {
+        console.log("Buzz");
+    }
+}
